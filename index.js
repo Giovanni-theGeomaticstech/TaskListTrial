e17c90e716c91c3969be99e0cb20bb1de78b1840
Gio <gio.harvey14@gmail.com>
2021-01-04 16:22:21 -0500

    files

---
+++ index.js
@@ -0,0 +1,33 @@
+// Here we are going to define a function to add our items
+function addItem(){
+    // The value task_info is the input box, we add .value to get the text in the input box
+    var task_info = document.getElementById("task")
+
+    // This is the task area in which we will add the new tasks
+    var task_Area = document.getElementById("taskArea") 
+    
+    var newTaskOuter = document.createElement("DIV") // Outer Container for tasks
+    var newTask = document.createElement("DIV") // Task that is added
+    var x_item = document.createElement("BUTTON") // X for remove the added tasks
+
+    x_item.innerHTML = "&#10006" // `X`
+    x_item.classList.add("remove_item")  
+    x_item.onclick = function(){ // Here we assign the onclick function
+        removeItem(this) // We are going to use this function to remove a tasks
+    }
+
+    newTask.innerHTML = task_info.value 
+    task_info.value = "" // We clear the input box after getting the value from it
+
+    newTask.classList.add("addedTask") // Adding the class addedTask to the newly created `Task`
+
+    newTaskOuter.append(newTask, x_item) // Here we add the new task and the `x` to the `Task Container`
+    task_Area.appendChild(newTaskOuter) // Here we add the outer task container to the `main Task Area Container`
+}
+
+// This function here removes the item
+function removeItem(){
+    parentTask = event.target.parentElement
+    parentTask.remove()
+    alert("Task will be removed")
+}
